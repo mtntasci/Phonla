725d00d6720c9236053d87c19f8e877efc6eb62a
mtntasci <mtntasci@gmail.com>
2026-08-14 18:41:19 +0300

    feat: initialize iOS project structure with documentation and basic scaffolding

---
+++ apps/Photon/Photon/PhotonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotonApp.swift
+//  Photon
+//
+//  Created by Metin TASCI on 14.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct PhotonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apps/Photon/PhotonTests/PhotonTests.swift
@@ -0,0 +1,19 @@
+//
+//  PhotonTests.swift
+//  PhotonTests
+//
+//  Created by Metin TASCI on 14.08.2026.
+//
+
+import Testing
+@testable import Photon
+
+struct PhotonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
