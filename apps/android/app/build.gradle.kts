1d7990b6ef30a234f5821e7f9a2c79cad7a56e6e
mtntasci <mtntasci@gmail.com>
2026-09-25 07:29:39 +0300

    chore: update version code to 9 and version name to 1.0 for Android and iOS

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.alafteknoloji.photon"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 9
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
